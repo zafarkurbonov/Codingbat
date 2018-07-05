1d5405228a2939cc476e1caf9c636de87d9b3ede
Zafar <Zafarchik29@gmail.com>
2018-07-05 15:12:38 +0500

    Added Solutions

--- java/Warmup_1.java
+++ java/Warmup_1.java
@@ -1,6 +1,39 @@
 public class Warmup_1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+   /* The parameter weekday is true if it is a weekday,
+    and the parameter vacation is true if we are on
+    vacation. We sleep in if it is not a weekday
+    or we're on vacation. Return true if we sleep in.
+
+    sleepIn(false, false) ? true
+    sleepIn(true, false) ? false
+    sleepIn(false, true) ? true
+*/
+   public boolean sleepIn(boolean weekday, boolean vacation) {
+       return vacation || !weekday ? true:false;
+   }
+
+
+
+   /* We have two monkeys, a and b, and the parameters
+    aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if
+    neither of them is smiling. Return true if we are in trouble.
+
+    monkeyTrouble(true, true) ? true
+    monkeyTrouble(false, false) ? true
+    monkeyTrouble(true, false) ? false*/
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return(aSmile && bSmile || !aSmile && !bSmile)?true:false;
     }
+
+
+
+
+
+
+
+
+
 }
