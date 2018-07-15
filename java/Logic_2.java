3a96bdce193f31470b1f44ed8814b6d21dd7bac8
Zafar Kurbonov <Zafarchik29@gmail.com>
2018-07-15 22:03:13 +0500

    Update Logic_2.java
    Added Solutions to Logic 2

--- java/Logic_2.java
+++ java/Logic_2.java
@@ -1,2 +1,25 @@
 public class Logic_2 {
+  
+   /* Given an array of ints, return true if 6 appears as either the first or last element in
+    the array. The array will be length 1 or more.
+    firstLast6([1, 2, 6]) ? true
+    firstLast6([6, 1, 2, 3]) ? true
+    firstLast6([13, 6, 1, 2, 3]) ? false*/
+
+    public boolean firstLast6(int[] nums) {
+        return (nums[0] == 6 || nums[nums.length - 1] == 6) ? true : false;
+    }
+
+
+  /*  Given an array of ints, return true if the array is length 1 or more, and the first element
+    and the last element are equal.
+    sameFirstLast([1, 2, 3]) ? false
+    sameFirstLast([1, 2, 3, 1]) ? true
+    sameFirstLast([1, 2, 1]) ? true */
+
+    public boolean sameFirstLast(int[] nums) {
+        return (nums.length >= 1 && nums[0] == nums[nums.length - 1]) ? true : false;
+    }
+  
+  MUST MODIFy ON THE CONDITION OF CHANGEE
 }
